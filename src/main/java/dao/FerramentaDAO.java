3057f47faa64c7c7ca1530c3ce531a2f04748204
Rennan <Rennan@10.150.65.232>
2024-05-14 19:46:51 -0300

    feat: implementei o CRUD na classe FerramentaDAO

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -6,25 +6,24 @@ import modelo.Ferramenta;
 
 public class FerramentaDAO {
 
-    
     public ArrayList<Ferramenta> ListaFerramentas = new ArrayList<>();
 
     public ArrayList<Ferramenta> getMinhaLista() {
-        
+
         ListaFerramentas.clear();
-        
+
         try {
             Statement stmt = this.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas");
             while (res.next()) {
-                
+
                 int id = res.getInt("id_ferramentas");
                 String nome = res.getString("nome");
                 String marca = res.getString("marca");
                 double custoAquisicao = Double.parseDouble(res.getString("custoAquisicao"));
-                
+
                 Ferramenta objeto = new Ferramenta(id, nome, marca, custoAquisicao);
-                
+
                 ListaFerramentas.add(objeto);
             }
             stmt.close();
@@ -34,7 +33,7 @@ public class FerramentaDAO {
         }
         return ListaFerramentas;
     }
-    
+
     public int maiorId() {
         int maiorId = 0;
         try {
@@ -48,7 +47,7 @@ public class FerramentaDAO {
         }
         return maiorId;
     }
-    
+
     public Connection getConexao() {
 
         Connection connection = null;
@@ -78,5 +77,79 @@ public class FerramentaDAO {
             System.out.println("Não foi possível conectar...");
             return null;
         }
+    }
+
+    public boolean inserirFerramentaBD(Ferramenta objeto) {
+        String sql = "INSERT INTO tb_ferramentas(id_ferramentas,nome,marca,custoAquisicao) VALUES(?,?,?,?)";
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setString(3, objeto.getMarca());
+            stmt.setDouble(4, objeto.getCustoAquisicao());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+        } catch (SQLException erro) {
+            System.out.println("Erro: " + erro);
+            throw new RuntimeException(erro);
+        }
+    }
+
+    public boolean apagarFerramentaBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+
+            stmt.executeUpdate("DELETE FROM tb_ferramentas WHERE id_ferramentas =" + id);
+
+            stmt.close();
+        } catch (SQLException erro) {
+            System.out.println("Erro: " + erro);
+        }
+        return true;
+    }
+
+    public boolean alterarFerramentaBD(Ferramenta objeto) {
+        String sql = "UPDATE tb_ferramentas set nome = ?, marca = ?, custoAquisicao = ? WHERE id_ferramentas = ?";
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setString(2, objeto.getMarca());
+            stmt.setDouble(3, objeto.getCustoAquisicao());
+            stmt.setInt(4, objeto.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            System.out.println("Erro: " + erro);
+            throw new RuntimeException(erro);
+        }
+    }
+
+    public Ferramenta carregarFerramenta(int id) {
+        Ferramenta objeto = new Ferramenta();
+        objeto.setId(id);
+        try {
+            Statement stmt = this.getConexao().createStatement();
+
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas WHERE id_ferramentas = " + id);
+            res.next();
+
+            objeto.setNome(res.getString("nome"));
+            objeto.setMarca(res.getString("marca"));
+            objeto.setCustoAquisicao(res.getDouble("custoAquisicao"));
+
+            stmt.close();
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+        }
+        return objeto;
     }
 }
