df160fcf97b7a5a9b6aed2dcd362c86fd012a7c8
1072411389 <1072411389@UNS11DT098810.animaedu.intranet>
2024-05-14 19:24:04 -0300

    feat: implementei os contrutores, os get's e os set's da classe Ferramentas.

--- src/main/java/modelo/Ferramenta.java
+++ src/main/java/modelo/Ferramenta.java
@@ -1,5 +1,52 @@
 package modelo;
 
 public class Ferramenta {
-    
+
+    private String nome, marca;
+    private double custoAquisicao
+    private int id;
+
+    public Ferramenta() {
+
+    }
+
+    public Ferramenta(String nome, String marca, double custoAquisicao, int id) {
+        this.nome = nome;
+        this.marca = marca;
+        this.custoAquisicao = custoAquisicao;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCustoAquisicao() {
+        return custoAquisicao;
+    }
+
+    public void setCustoAquisicao(double custoAquisicao) {
+        this.custoAquisicao = custoAquisicao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
