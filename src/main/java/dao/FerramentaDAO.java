97903fbc11cb7bac9685b7b0214d77b3e38badca
Rennan <Rennan@RENNAN>
2024-05-14 13:42:52 -0300

    feat: implementei o metodo getConexao para conectar o projeto ao banco de dados

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,5 +1,37 @@
 package dao;
 
+import java.sql.*;
+
 public class FerramentaDAO {
-    
+    public Connection getConexao() {
+
+        Connection connection = null;
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            String server = "localhost";
+            String database = "Database";
+            String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "root";
+
+            connection = DriverManager.getConnection(url, user, password);
+
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: Não Conectado!");
+            }
+            return connection;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("O driver não foi encontrado.");
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Não foi possível conectar...");
+            return null;
+        }
+    }
 }
+    
