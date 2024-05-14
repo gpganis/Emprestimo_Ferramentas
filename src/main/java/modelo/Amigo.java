5ded60e8b87acbea338742789e082a98cacb364b
1072411389 <1072411389@UNS11DT098810.animaedu.intranet>
2024-05-14 19:16:23 -0300

    feat: implementei os contrutores, os get's e os set's da classe Amigo.

--- src/main/java/modelo/Amigo.java
+++ src/main/java/modelo/Amigo.java
@@ -1,5 +1,42 @@
 package modelo;
 
 public class Amigo {
-    
+
+    private int id;
+    private String nome, telefone;
+
+    public Amigo() {
+
+    }
+
+    public Amigo(int id, String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 }
