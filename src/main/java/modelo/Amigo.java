b5756ca086f16ac83e3f00293fee4e5647c37ff4
1072411389 <1072411389@UNS11DT098810.animaedu.intranet>
2024-05-14 20:03:49 -0300

    feat: implementei o CRUD na classe Amigo

--- src/main/java/modelo/Amigo.java
+++ src/main/java/modelo/Amigo.java
@@ -1,18 +1,23 @@
 package modelo;
 
+import dao.AmigoDAO;
+import java.util.ArrayList;
+
 public class Amigo {
 
     private int id;
     private String nome, telefone;
+    private AmigoDAO dao;
 
     public Amigo() {
-
+        dao = new AmigoDAO();
     }
 
     public Amigo(int id, String nome, String telefone) {
         this.nome = nome;
         this.telefone = telefone;
         this.id = id;
+        dao = new AmigoDAO();
     }
 
     public int getId() {
@@ -37,6 +42,32 @@ public class Amigo {
 
     public void setTelefone(String telefone) {
         this.telefone = telefone;
+    }
+
+    public ArrayList<Amigo> getListaAmigos() {
+        return dao.getMinhaLista();
+    }
+
+    public boolean inserirAmigo(String nome, String telefone) {
+        id = dao.maiorId() + 1;
+        Amigo objeto = new Amigo(id, nome, telefone);
+        dao.inserirAmigoBD(objeto);
+        return true;
+    }
+
+    public boolean apagarAmigo(int id) {
+        dao.apagarAmigoBD(id);
+        return true;
+    }
+
+    public boolean alterarAmigo(int id, String nome, String telefone) {
+        Amigo objeto = new Amigo(id, nome, telefone);
+        dao.alterarAmigoBD(objeto);
+        return true;
+    }
+
+    public Amigo carregarAmigo(int id) {
+        return dao.carregarAmigoBD(id);
     }
 
 }
