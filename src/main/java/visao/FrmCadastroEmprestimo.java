92edf39ab68217c82b729da492d2fcc475431195
Rennan <Rennan@RENNAN>
2024-05-22 20:36:48 -0300

    feat: Repaginei a interface de cadastro de emprestimos.

--- src/main/java/visao/FrmCadastroEmprestimo.java
+++ src/main/java/visao/FrmCadastroEmprestimo.java
@@ -47,42 +47,28 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        JBCancelar = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        JLTitulo = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
-        JCBAmigo = new javax.swing.JComboBox<>();
-        jLabel1 = new javax.swing.JLabel();
-        JTFAmigo = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         JTFDataEmp = new javax.swing.JTextField();
+        JCBAmigo = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         JTFDataDev = new javax.swing.JTextField();
         JBCadastrar = new javax.swing.JButton();
+        JBCancelar = new javax.swing.JButton();
+        JTFFerramenta = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        JBAdicionar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        JLTitulo = new javax.swing.JLabel();
+        JTFAmigo = new javax.swing.JTextField();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
-        JBCancelar.setText("Fechar");
-        JBCancelar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JBCancelarActionPerformed(evt);
-            }
-        });
+        jLabel2.setText("Data do Empréstimo:");
 
-        jLabel4.setText("Ferramentas:");
-
-        JLTitulo.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        JLTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        JLTitulo.setText("Cadastro de Empréstimos");
-
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(jList1);
+        JTFDataEmp.setEditable(false);
 
         JCBAmigo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecionar" }));
         JCBAmigo.addActionListener(new java.awt.event.ActionListener() {
@@ -90,19 +76,6 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
                 JCBAmigoActionPerformed(evt);
             }
         });
-
-        jLabel1.setText("Amigo:");
-
-        JTFAmigo.setEditable(false);
-        JTFAmigo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                JTFAmigoActionPerformed(evt);
-            }
-        });
-
-        jLabel2.setText("Data do Empréstimo:");
-
-        JTFDataEmp.setEditable(false);
 
         jLabel3.setText("Data de Devolução:");
 
@@ -130,35 +103,93 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
             }
         });
 
+        JBCancelar.setText("Fechar");
+        JBCancelar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBCancelarActionPerformed(evt);
+            }
+        });
+
+        JTFFerramenta.setEditable(false);
+
+        jLabel4.setText("Ferramentas:");
+
+        JBAdicionar.setText("Adicionar");
+        JBAdicionar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBAdicionarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Amigo:");
+
+        JLTitulo.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        JLTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        JLTitulo.setText("Cadastro de Empréstimos");
+
+        JTFAmigo.setEditable(false);
+        JTFAmigo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFAmigoActionPerformed(evt);
+            }
+        });
+
+        jTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "ID", "Ferramenta"
+            }
+        ));
+        jTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTableMouseClicked(evt);
+            }
+        });
+        jScrollPane3.setViewportView(jTable);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 84, Short.MAX_VALUE)
+                .addGap(0, 0, Short.MAX_VALUE)
                 .addComponent(JBCancelar)
                 .addGap(84, 84, 84)
                 .addComponent(JBCadastrar)
                 .addGap(80, 80, 80))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addGap(40, 40, 40)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1)
-                    .addComponent(JLTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, 320, Short.MAX_VALUE)
-                    .addComponent(JTFDataEmp)
-                    .addComponent(JTFAmigo)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(JCBAmigo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(JTFDataDev)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel2)
-                            .addComponent(jLabel3))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addGap(40, 40, 40))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(JTFFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(JBAdicionar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(JLTitulo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(JTFDataEmp)
+                            .addComponent(JTFAmigo)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(JCBAmigo, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(JTFDataDev)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel2)
+                                    .addComponent(jLabel3))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(40, 40, 40))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -174,8 +205,12 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
                 .addGap(25, 25, 25)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JTFFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(JBAdicionar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(JTFDataEmp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -190,31 +225,16 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
                 .addGap(40, 40, 40))
         );
 
-        setSize(new java.awt.Dimension(422, 540));
+        setSize(new java.awt.Dimension(417, 615));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void JBCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCancelarActionPerformed
-        // TODO add your handling code here:
-        this.dispose();
-    }//GEN-LAST:event_JBCancelarActionPerformed
-
     private void JCBAmigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JCBAmigoActionPerformed
         // TODO add your handling code here
-        String nomeSelecionado = (String) JCBAmigo.getSelectedItem();
-        JTFAmigo.setText(nomeSelecionado);
     }//GEN-LAST:event_JCBAmigoActionPerformed
-
-    private void JTFAmigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFAmigoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_JTFAmigoActionPerformed
 
     private void JTFDataDevMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JTFDataDevMouseClicked
         // TODO add your handling code here:
-        if (countData == true){
-            JTFDataDev.setText("");
-            countData = false;
-        }
     }//GEN-LAST:event_JTFDataDevMouseClicked
 
     private void JTFDataDevActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFDataDevActionPerformed
@@ -223,52 +243,28 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
 
     private void JTFDataDevKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_JTFDataDevKeyPressed
         // TODO add your handling code here:
-        if (countData == true){
-            JTFDataDev.setText("");
-            countData = false;
-        }
     }//GEN-LAST:event_JTFDataDevKeyPressed
 
     private void JBCadastrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCadastrarActionPerformed
         // TODO add your handling code here:
-        try {
-            String regex = "\\d{4}-\\d{2}-\\d{2}";
-            int idAmg = 0;
-            Date dataEmprestimo = Util.dataAtual();            
-            boolean entregue = false;
-            Date dataDevolucao = null;
 
-            if ("".equals(this.JTFAmigo.getText())){
-                throw new Mensagens("Primeiro selecione um Amigo.");
-            } else {
-                idAmg = AmigoDAO.getIdPeloNome(JTFAmigo.getText());
-            }
-            
-            if (this.JTFDataDev.getText().matches(regex)) {
-                dataDevolucao = Util.stringParaDateSQL(JTFDataDev.getText());
-                                                                                                                                                        
-            } else {
-                throw new Mensagens("""
-                                    Data de Devolução deve conter o seguite formato: 
-                                                    yyyy-MM-dd
-                                    """);
-            }
-
-            if (this.objEmprestimo.inserirEmprestimo(dataEmprestimo, dataDevolucao, entregue, idAmg)) {
-                JOptionPane.showMessageDialog(rootPane, "Empréstimo Cadastrado com Sucesso!");
-                this.JTFAmigo.setText("");
-                this.JTFDataDev.setText("");
-
-            }
-            //System.out.println(this.objEmprestimo.getListaFerramentas().toString());
-        } catch (Mensagens erro) {
-            JOptionPane.showMessageDialog(null, erro.getMessage());
-        } catch (TextFormat.ParseException ex) {
-            Logger.getLogger(FrmCadastroEmprestimo.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (java.text.ParseException ex) {
-            Logger.getLogger(FrmCadastroEmprestimo.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }//GEN-LAST:event_JBCadastrarActionPerformed
+
+    private void JBCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCancelarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JBCancelarActionPerformed
+
+    private void JBAdicionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBAdicionarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JBAdicionarActionPerformed
+
+    private void JTFAmigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFAmigoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JTFAmigoActionPerformed
+
+    private void jTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTableMouseClicked
 
     public static void main(String args[]) {
 
@@ -280,6 +276,7 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBAdicionar;
     private javax.swing.JButton JBCadastrar;
     private javax.swing.JButton JBCancelar;
     private javax.swing.JComboBox<String> JCBAmigo;
@@ -287,11 +284,12 @@ public class FrmCadastroEmprestimo extends javax.swing.JFrame {
     private javax.swing.JTextField JTFAmigo;
     private javax.swing.JTextField JTFDataDev;
     private javax.swing.JTextField JTFDataEmp;
+    private javax.swing.JTextField JTFFerramenta;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JList<String> jList1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable;
     // End of variables declaration//GEN-END:variables
 }
