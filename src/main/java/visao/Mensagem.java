dc6a18e2a5a742f8bfaa9ff3f916fa00d5c3f269
Rennan <Rennan@10.150.109.138>
2024-06-04 14:33:24 -0300

    docs: Arrumei os comentarios da classe Mensagem.java

--- src/main/java/visao/Mensagem.java
+++ src/main/java/visao/Mensagem.java
@@ -2,8 +2,14 @@ package visao;
 
 public class Mensagem extends Exception {
 
-    // Construtor da classe Mensagem que recebe uma mensagem como parâmetro
+    /**
+     * Construtor da classe Mensagem que recebe uma mensagem como parâmetro
+     * @param msg 
+     */
     Mensagem(String msg) {
-        super(msg); // Chama o construtor da classe Exception passando a mensagem como argumento
+        super(msg); 
+        /**
+         * Chama o construtor da classe Exception passando a mensagem como argumento
+         */
     }
 }
